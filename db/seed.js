4b422926b4500832b0476d1fefdcb96f2b18ce01
Elwin <elwin.carlos09@gmail.com>
2021-05-28 19:16:12 +0100

    comment out references to JSON file

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,7 @@
 const db = connect("mongodb://localhost:27017/dev_db");
 
-const { readJSON } = require('./json-manager')
-const cardSeeds = readJSON()
+// const { readJSON } = require('./json-manager')
+// const cardSeeds = readJSON()
 
 db.cards.drop();
 
